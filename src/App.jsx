7dfd5804823635a457a1d8a4501d0f69de989769
apolline-diaz <apolline.diaz@gmail.com>
2024-05-25 12:30:52 +0200

    Update Homepage Text

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 import "./App.css";
 import Navbar from "./components/Navbar.jsx";
 import Timeline from "./components/Timeline.jsx";
@@ -11,6 +13,11 @@ import Github from "/assets/github.png";
 
 function App() {
   const [count, setCount] = useState(0);
+  useEffect(() => {
+    AOS.init({
+      duration: 1000, // Durée de l'animation en millisecondes
+    });
+  }, []);
 
   return (
     <>
@@ -18,9 +25,13 @@ function App() {
       <Navbar />
       <main>
         {/* HOME SECTION */}
+
         <section id="home" className="h-full w-full">
           <div className='bg-[url("/assets/home_background.png")] h-screen w-full bg-cover bg-center'>
-            <div className="container justify-center max-w-[700px] mx-auto px-5 py-20 text-left m-auto flex flex-col gap-2">
+            <div
+              className="container justify-center items-center h-full max-w-[700px] mx-auto px-5 py-20 text-left m-auto flex flex-col gap-2 "
+              data-aos="fade-up"
+            >
               <h2 className="font-light text-4xl md:text-5xl xl:text-6xl mb-3">
                 Hello, <br />
                 I'm Apolline Diaz
@@ -30,29 +41,22 @@ function App() {
                   Développeuse full stack
                 </h2>
               </div>
-              {/* <p className="font-medium text-xl sm:text-l md:text-xl lg:text-2xl">
-                En recherche d'alternance
-              </p> */}
-              {/* <p className="font-light text-xl sm:text-l md:text-xl lg:text-2xl gradient-text running-text">
-                En recherche d'alternance
-              </p> */}
+
               <div>
-                <p className="font-light text-l sm:text-md md:text-xl w-50 max-w-4xl mb-2">
+                <p className="font-light text-justify text-l sm:text-md md:text-xl w-50 max-w-4xl mb-2">
                   Passionnée par la création audiovisuelle et multimédia, je me
                   suis lancée dans le développement, après plusieurs années dans
-                  le milieu du cinéma. Je développe des projets orientés
-                  front-end, en autodidacte. J'aime travailler en équipe et
-                  continuer à apprendre de nouvelles technologies, grâce à ma
-                  formation à Ada Tech School.
+                  le milieu du cinéma. Grâce à ma formation à Ada Tech School,
+                  j'ai appris à réaliser des applications web et mobiles.
+                  Aujourd'hui, je développe de nouvelles compétences pour me
+                  spécialiser dans la programmation orientée vers le design, la
+                  valeur utilisateur et l'expérience interactive.
                 </p>
               </div>
               <div className="space-x-3 flex flex-cols items-center">
                 <button
                   onClick={() =>
-                    window.open(
-                      "./assets/CV-ApollineDiaz-portfolio.pdf",
-                      "_blank"
-                    )
+                    window.open("./assets/CV-ApollineDiaz.pdf", "_blank")
                   }
                   className="bg-black text-white font-light  p-2 transition duration-200 hover:opacity-50"
                 >
