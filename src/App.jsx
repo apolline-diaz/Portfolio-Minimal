36be2171c55af16a0dc78d0404b3c76d144cda96
apolline-diaz <apolline.diaz@gmail.com>
2024-06-03 12:30:16 +0200

    Add project Pokedex and justify text

--- src/App.jsx
+++ src/App.jsx
@@ -26,24 +26,24 @@ function App() {
       <main>
         {/* HOME SECTION */}
 
-        <section id="home" className="h-full w-full">
-          <div className='bg-[url("/assets/home_background.png")] h-screen w-full bg-cover bg-center'>
+        <section id='home' className='h-full w-full'>
+          <div className='bg-[url("/assets/home_background.png")] h-screen w-full bg-cover bg-center '>
             <div
-              className="container justify-center items-center h-full max-w-[700px] mx-auto px-5 py-20 text-left m-auto flex flex-col gap-2 "
-              data-aos="fade-up"
+              className='container justify-center items-center h-full max-w-[700px] mx-auto px-10 py-20 text-left m-auto flex flex-col gap-2 '
+              data-aos='fade-up'
             >
-              <h2 className="font-light text-4xl md:text-5xl xl:text-6xl mb-3">
+              <h2 className='font-light text-4xl md:text-5xl xl:text-6xl mb-3'>
                 Hello, <br />
                 I'm Apolline Diaz
               </h2>
               <div>
-                <h2 className="font-bold text-2xl sm:text-2xl md:text-3xl">
+                <h2 className='font-bold text-2xl sm:text-2xl md:text-3xl'>
                   Développeuse full stack
                 </h2>
               </div>
 
               <div>
-                <p className="font-light text-justify text-l sm:text-md md:text-xl w-50 max-w-4xl mb-2">
+                <p className='font-light text-justify text-l sm:text-md md:text-xl w-50 max-w-4xl mb-2'>
                   Passionnée par la création audiovisuelle et multimédia, je me
                   suis lancée dans le développement, après plusieurs années dans
                   le milieu du cinéma. Grâce à ma formation à Ada Tech School,
@@ -53,12 +53,12 @@ function App() {
                   valeur utilisateur et l'expérience interactive.
                 </p>
               </div>
-              <div className="space-x-3 flex flex-cols items-center">
+              <div className='space-x-3 flex flex-cols items-center'>
                 <button
                   onClick={() =>
-                    window.open("./assets/CV-ApollineDiaz.pdf", "_blank")
+                    window.open("./assets/CV_ApollineDiaz.pdf", "_blank")
                   }
-                  className="bg-black text-white font-light  p-2 transition duration-200 hover:opacity-50"
+                  className='bg-black text-white font-light  p-2 transition duration-200 hover:opacity-50'
                 >
                   CV
                 </button>
@@ -68,25 +68,25 @@ function App() {
                   </button>
                 </a> */}
                 <a
-                  href="https://github.com/apolline-diaz"
-                  target="_blank"
-                  rel="noopener noreferrer"
+                  href='https://github.com/apolline-diaz'
+                  target='_blank'
+                  rel='noopener noreferrer'
                 >
                   <img
                     src={Github}
-                    alt="Github"
-                    className="cursor-pointer  w-8 h-8 transition duration-200 hover:opacity-50 "
+                    alt='Github'
+                    className='cursor-pointer  w-8 h-8 transition duration-200 hover:opacity-50 '
                   />
                 </a>
                 <a
-                  href="https://www.linkedin.com/in/apolline-diaz-938783153/"
-                  target="_blank"
-                  rel="noopener noreferrer"
+                  href='https://www.linkedin.com/in/apolline-diaz-938783153/'
+                  target='_blank'
+                  rel='noopener noreferrer'
                 >
                   <img
                     src={LinkedIn}
-                    alt="LinkedIn"
-                    className="cursor-pointer w-8 h-8 transition duration-200 hover:opacity-50"
+                    alt='LinkedIn'
+                    className='cursor-pointer w-8 h-8 transition duration-200 hover:opacity-50'
                   />
                 </a>
               </div>
@@ -95,12 +95,12 @@ function App() {
         </section>
         {/* PROJECTS SECTION */}
         <section
-          id="projects"
-          className="p-5 justify-center items-center max-w-[700px] mx-auto"
+          id='projects'
+          className='p-5 justify-center items-center max-w-[700px] mx-auto'
         >
-          <div className="container  m-auto px-4">
-            <h2 className="text-4xl font-light my-5">Projets</h2>
-            <p className="text-base md:text-lg text-justify mb-5">
+          <div className='container  m-auto px-4'>
+            <h2 className='text-4xl font-light my-5'>Projets</h2>
+            <p className='text-base font-light md:text-lg text-justify mb-5'>
               Chaque nouveau projet est une opportunité de prendre en main une
               nouvelle technologie, se l'approprier et en exploiter son
               potentiel. Je me consacre autant à la conception et l'amélioration
@@ -108,18 +108,18 @@ function App() {
               traitement des bases de données. Avant de faire la déco d'une
               maison, il ne faut pas oublier ses fondements!
             </p>
-            <div className="grid grid-cols-1 sm:grid-col md:grid-col lg:grid-col gap-10">
+            <div className='grid grid-cols-1 sm:grid-col md:grid-col lg:grid-col gap-10'>
               <Portfolio />
             </div>
-            <h2 className="text-2xl font-light my-5">Compétences techniques</h2>
+            <h2 className='text-2xl font-light my-5'>Compétences techniques</h2>
             <Skills />
           </div>
         </section>
         {/* EXPERIENCE SECTION */}
-        <section id="experience" className="p-5 max-w-[700px] mx-auto">
-          <div className="container m-auto px-4 mb-10">
-            <h2 className="text-4xl font-light my-5">Expérience</h2>
-            <p className="text-base md:text-lg text-justify">
+        <section id='experience' className='p-5 max-w-[700px] mx-auto'>
+          <div className='container m-auto px-4 mb-10'>
+            <h2 className='text-4xl font-light my-5'>Expérience</h2>
+            <p className='text-base font-light md:text-lg text-justify'>
               Pendant mes années dans les salles de cinéma, j'avais une autre
               passion : partager des films peu connus du grand public, et
               trouver des moyens de les rendre plus accessibles. Internet était
@@ -133,7 +133,7 @@ function App() {
                     "_blank"
                   )
                 }
-                className="bg-black text-white font-light  p-1 transition duration-200 hover:opacity-50 cursor-pointer"
+                className='bg-black text-white font-light  p-1 transition duration-200 hover:opacity-50 cursor-pointer'
               >
                 portfolio
               </a>{" "}
@@ -143,15 +143,15 @@ function App() {
           <Timeline />
         </section>
         {/* CONTACT SECTION */}
-        <section id="contact" className="p-5 max-w-[700px] mx-auto">
-          <div className="container m-auto px-4 mb-10">
-            <h2 className="text-4xl font-light">Contact</h2>
+        <section id='contact' className='p-5 max-w-[700px] mx-auto'>
+          <div className='container m-auto px-4 mb-10'>
+            <h2 className='text-4xl font-light'>Contact</h2>
           </div>
           <Contact />
         </section>
         {/* FOOTER */}
-        <section className="py-10">
-          <div className="container m-auto px-4"></div>
+        <section className='py-10'>
+          <div className='container m-auto px-4'></div>
           <Footer />
         </section>
       </main>
--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,65 +1,92 @@
 export default [
-    {
-        title: "Application mobile",
-        description: "Application  sur Android (en cours) qui permet de découvrir des spots urbains insolites ou faire de belles photos. A partager avec sa communauté !",
-        imgUrl: "./assets/project-app-mobile.png",
-        stack:["Kotlin", "Spring Boot"],
-        linkGit: "https://github.com/apolline-diaz/App-mobile-Android-Urbanspots",
-        linkDemo: "",
-    },
-    {
-        title: "Site de référencement de cinéma",
-        description: "Projet personnel (en cours) de refonte d'un premier projet de site d'actualité cinéma, cette fois en React, Tailwind et avec MySQL.",
-        imgUrl: "./assets/project-movie-app.png",
-        stack:["HTML", "CSS", "React", "Tailwind", "MySQL", "API"],
-        linkGit: "https://github.com/apolline-diaz/Movie-App",
-        linkDemo: "https://apolline-diaz.github.io/Movie-App",
-    },
-    // {
-    //     title: "Site de référencement de cinéma",
-    //     description: "Ebauche d'un site mettant en avant l'actualité des films et séries, à partir de la base de données The Movie Database.",
-    //     imgUrl: "./assets/project-movie-website-1.png",
-    //     stack:["HTML", "CSS", "JavaScript", "API"],
-    //     linkGit: "https://github.com/apolline-diaz/Search-Movie-App",
-    //     linkDemo: "https://apolline-diaz.github.io/Search-Movie-App/",
-    // },
-    {
-        title: "Plateforme e-commerce",
-        description: "Plateforme de vente en ligne de meubles d'occasion, interagissant avec une base de données.",
-        imgUrl: "./assets/project-plateforme.png",
-        stack:["HTML", "CSS", "JavaScript", "React", "Express", "MySQL", "Sequelize"],
-        linkGit: "https://github.com/apolline-diaz/E-Shop-Website-Second-hand-furniture-Adopteunmeuble",
-        linkDemo: "https://vimeo.com/924123048?share=copy",
-    },
-    {
-        title: "Extension Chrome",
-        description: "L'espace de Nouvel onglet est transformé avec l'univers du studio Ghibli, dotée d'outils (To Do List, météo, etc.).",
-        imgUrl: "./assets/project-extension.png",
-        stack:["HTML", "CSS", "JavaScript", "API"],
-        linkGit: "https://github.com/apolline-diaz/Ghibli-New-Tab-Browser-Extension",
-        linkDemo: "https://vimeo.com/927429709?share=copy",
-    },
-    {
-        title: "Datavisualisation",
-        description: "Page web affichant les différences de productions d'électricité des pays en Europe, à partir de l'API Electricity Maps. Démo indisponible (API expirée)",
-        imgUrl: "./assets/project-datavisualisation.png",
-        stack:["HTML", "CSS", "JavaScript", "API"],
-        linkGit: "https://github.com/apolline-diaz/Datavisualisation-API-Electricity-Maps",
-    },
-    {
-        title: "Jeu vidéo Pico 8",
-        description: "Ebauche de jeu vidéo de plateforme sur la console Pico 8. Le concept : un citron doit cueillir les fruits de saison.",
-        imgUrl: "./assets/project-pico.png",
-        stack:["Lua"],
-        linkGit: "https://github.com/apolline-diaz/Pico-8-Video-game",
-        linkDemo: "https://vimeo.com/927425518",
-    },
-    {
-        title: "Jeu vidéo Pico 8 Spécial Anniversaire",
-        description: "Projet personnel pour un anniversaire. Le concept : éliminer 27 chatons pour découvrir son cadeau.",
-        imgUrl: "./assets/project-pico-anniversaire.png",
-        stack:["Lua"],
-        linkGit: "https://github.com/apolline-diaz/Pico-8-Shooter-Birthday-Video-Game",
-        linkDemo: "https://lawrens-shyboi.itch.io/queen-of-bad-bitches",
-    },
-]
+  {
+    title: "Application mobile",
+    description:
+      "Application  sur Android (en cours) qui permet de découvrir des spots urbains insolites ou faire de belles photos. A partager avec sa communauté !",
+    imgUrl: "./assets/project-app-mobile.png",
+    stack: ["Kotlin", "Spring Boot"],
+    linkGit: "https://github.com/apolline-diaz/App-mobile-Android-Urbanspots",
+    linkDemo: "",
+  },
+  {
+    title: "Site de référencement de cinéma",
+    description:
+      "Projet personnel (en cours) de refonte d'un premier projet de site d'actualité cinéma, cette fois en React, Tailwind et avec MySQL.",
+    imgUrl: "./assets/project-movie-app.png",
+    stack: ["HTML", "CSS", "React", "Tailwind", "MySQL", "API"],
+    linkGit: "https://github.com/apolline-diaz/Movie-App",
+    linkDemo: "https://apolline-diaz.github.io/Movie-App",
+  },
+  // {
+  //     title: "Site de référencement de cinéma",
+  //     description: "Ebauche d'un site mettant en avant l'actualité des films et séries, à partir de la base de données The Movie Database.",
+  //     imgUrl: "./assets/project-movie-website-1.png",
+  //     stack:["HTML", "CSS", "JavaScript", "API"],
+  //     linkGit: "https://github.com/apolline-diaz/Search-Movie-App",
+  //     linkDemo: "https://apolline-diaz.github.io/Search-Movie-App/",
+  // },
+  {
+    title: "Plateforme e-commerce",
+    description:
+      "Plateforme de vente en ligne de meubles d'occasion, interagissant avec une base de données.",
+    imgUrl: "./assets/project-plateforme.png",
+    stack: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "React",
+      "Express",
+      "MySQL",
+      "Sequelize",
+    ],
+    linkGit:
+      "https://github.com/apolline-diaz/E-Shop-Website-Second-hand-furniture-Adopteunmeuble",
+    linkDemo: "https://vimeo.com/924123048?share=copy",
+  },
+  {
+    title: "Extension Chrome",
+    description:
+      "L'espace de Nouvel onglet est transformé avec l'univers du studio Ghibli, dotée d'outils (To Do List, météo, etc.).",
+    imgUrl: "./assets/project-extension.png",
+    stack: ["HTML", "CSS", "JavaScript", "API"],
+    linkGit:
+      "https://github.com/apolline-diaz/Ghibli-New-Tab-Browser-Extension",
+    linkDemo: "https://vimeo.com/927429709?share=copy",
+  },
+  //   {
+  //     title: "Datavisualisation",
+  //     description:
+  //       "Page web affichant les différences de productions d'électricité des pays en Europe, à partir de l'API Electricity Maps. Démo indisponible (API expirée)",
+  //     imgUrl: "./assets/project-datavisualisation.png",
+  //     stack: ["HTML", "CSS", "JavaScript", "API"],
+  //     linkGit:
+  //       "https://github.com/apolline-diaz/Datavisualisation-API-Electricity-Maps",
+  //   },
+  {
+    title: "Pokédex",
+    description:
+      "Encyclopédie numérique qui recense les Pokémon à partir de PokeAPI, avec recherche par nom.",
+    imgUrl: "./assets/project-pokedex.png",
+    stack: ["HTML", "CSS", "React", "API"],
+    linkGit: "https://github.com/apolline-diaz/Pokedex",
+    linkDemo: "https://apolline-diaz.github.io/Pokedex/",
+  },
+  // {
+  //     title: "Jeu vidéo Pico 8",
+  //     description: "Ebauche de jeu vidéo de plateforme sur la console Pico 8. Le concept : un citron doit cueillir les fruits de saison.",
+  //     imgUrl: "./assets/project-pico.png",
+  //     stack:["Lua"],
+  //     linkGit: "https://github.com/apolline-diaz/Pico-8-Video-game",
+  //     linkDemo: "https://vimeo.com/927425518",
+  // },
+  {
+    title: "Jeu vidéo Pico 8 Spécial Anniversaire",
+    description:
+      "Projet personnel pour un anniversaire. Le concept : éliminer 27 chatons pour découvrir son cadeau.",
+    imgUrl: "./assets/project-pico-anniversaire.png",
+    stack: ["Lua"],
+    linkGit:
+      "https://github.com/apolline-diaz/Pico-8-Shooter-Birthday-Video-Game",
+    linkDemo: "https://lawrens-shyboi.itch.io/queen-of-bad-bitches",
+  },
+];
